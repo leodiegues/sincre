61fd76ff78d468283fa0eee71fb96bac8b6c15b4
leonardodiegues <dscleo@gmail.com>
2018-03-29 13:04:55 -0300

    final changes

--- sincre.R
+++ sincre.R
@@ -3,28 +3,20 @@
 
 require(dplyr)
 
-# monta final das URLs ---------------------------------------------------------
-url_end <- c(paste(
-  seq(52, 66, 1), 
-  seq(2000, 2014, 1),
-  sep = "-"
-  )
-)
-
+# Monta URLs de requisição:
 request_url <- c(
   paste0(
     rep(
-      'http://obmigra.mte.gov.br/index.php/microdados/sincre/itemlist/category/',
-      length(url_end)
+      'http://obmigra.mte.gov.br/index.php/microdados/sincre?start=', 2
     ),
-    url_end
+    seq(0, 10, 10)
   )
 )
 
+# Raspa os links das tabelas:
 get_page_links <- function(urls) {
-  xpath_a <- "//*[@id='k2Container']/div[1]/div/div[1]/div[4]/div/ul/li/a"
-  xpath_last_update <- "//*[@id='k2Container']/div[1]/div/div[2]/ul/li[1]/text()"
-  perm_df <- data.frame(stringsAsFactors = F)
+  xpath_a <- "//*[@id='k2Container']/div[2]/div/div[1]/div[4]/div/ul/li/a"
+  data <- data.frame(stringsAsFactors = F)
     purrr::map_df(urls, function (x) {
       print(x)
       html <- httr::GET(x) %>%
@@ -37,21 +29,15 @@ get_page_links <- function(urls) {
         rvest::html_nodes(xpath = xpath_a) %>% 
         rvest::html_text() %>% 
         stringr::str_trim()
-      last_update <- xml2::read_html(html) %>% 
-        rvest::html_nodes(xpath = xpath_last_update) %>% 
-        rvest::html_text() %>% 
-        strptime(format = "%d/%m/%y") %>% 
-        as.POSIXct()
       temp <- data.frame(
         name             = name, 
         url              = url,
-        last_update      = last_update,
         stringsAsFactors = F
       )
       data <- bind_rows(data, temp) %>% 
         mutate(clean_name = stringr::str_replace(name, "^SINCRE ", "") %>% 
                  stringr::str_replace("\\(.*", "") %>% 
-                 stringr::str_replace("_ DATA DE |_DATA DE ", "") %>% 
+                 stringr::str_replace("_ DATA DE |_DATA DE |- DATA DE ", "") %>% 
                  stringr::str_trim() %>% 
                  stringr::str_replace("\\s", "_")
       )
@@ -60,6 +46,7 @@ get_page_links <- function(urls) {
 
 page_links <- get_page_links(urls = request_url)
 
+# Baixa os arquivos dos links raspados:
 download <- function(data) {
   if (!dir.exists("data")) {
     dir.create("data")
@@ -67,10 +54,10 @@ download <- function(data) {
     cat('directory already exists\n')
   }
   for (i in 1:nrow(data)) {
-    print(paste0("downloading ", file_name, "..."))
-    file_name <- data[i, 4]
+    file_name <- data[i, 3]
     file_path <- paste0('data/', file_name, '.txt')
-    if (!file.exists(file_name)) {
+    print(paste0("downloading ", file_name, "..."))
+    if (!file.exists(file_path)) {
       download.file(data[i, 2], file_path)
     } else {
       print('file has already been downloaded')
@@ -101,12 +88,12 @@ bind_tables <- function (files_list) {
 
 entrada <- bind_tables(files_list = files_list_entrada)
 readr::write_delim(x     = entrada, 
-                   path  = 'entrada.txt', 
+                   path  = 'binded_tables/entrada.txt', 
                    delim = ';', 
                    na    = '')
 
 registro <- bind_tables(files_list = files_list_registro)
 readr::write_delim(x     = registro, 
-                   path  = 'registro.txt', 
+                   path  = 'binded_tables/registro.txt', 
                    delim = ';', 
                    na    = '')
