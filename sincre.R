6991fbe44c3eee0a7e66d336785a1d7c35b60010
Leonardo Diegues <leonardo.diegues@grupofolha.com.br>
2018-04-02 10:33:38 -0300

    final commit

--- sincre.R
+++ sincre.R
@@ -15,25 +15,34 @@ request_url <- c(
 
 # Raspa os links das tabelas:
 get_page_links <- function(urls) {
+  
   xpath_a <- "//*[@id='k2Container']/div[2]/div/div[1]/div[4]/div/ul/li/a"
+  
   data <- data.frame(stringsAsFactors = F)
+  
     purrr::map_df(urls, function (x) {
+      
       print(x)
+      
       html <- httr::GET(x) %>%
         httr::content('text')
+      
       url <- xml2::read_html(html) %>% 
         rvest::html_nodes(xpath = xpath_a) %>% 
         rvest::html_attr("href") %>% 
         paste0('http://obmigra.mte.gov.br', .)
+      
       name <- xml2::read_html(html) %>% 
         rvest::html_nodes(xpath = xpath_a) %>% 
         rvest::html_text() %>% 
         stringr::str_trim()
+      
       temp <- data.frame(
         name             = name, 
         url              = url,
         stringsAsFactors = F
       )
+      
       data <- bind_rows(data, temp) %>% 
         mutate(clean_name = stringr::str_replace(name, "^SINCRE ", "") %>% 
                  stringr::str_replace("\\(.*", "") %>% 
@@ -41,28 +50,46 @@ get_page_links <- function(urls) {
                  stringr::str_trim() %>% 
                  stringr::str_replace("\\s", "_")
       )
+      
   })
+    
 }
 
 page_links <- get_page_links(urls = request_url)
 
 # Baixa os arquivos dos links raspados:
 download <- function(data) {
+  
   if (!dir.exists("data")) {
+    
     dir.create("data")
+    
   } else {
+    
     cat('directory already exists\n')
+    
   }
+  
   for (i in 1:nrow(data)) {
+    
     file_name <- data[i, 3]
+    
     file_path <- paste0('data/', file_name, '.txt')
+    
     print(paste0("downloading ", file_name, "..."))
+    
     if (!file.exists(file_path)) {
+      
       download.file(data[i, 2], file_path)
+      
     } else {
+      
       print('file has already been downloaded')
+      
     }
+    
   }
+  
 }
 
 download(page_links)
@@ -71,9 +98,13 @@ files_list_entrada <- list.files('data/', 'ENTRADA.txt$')
 files_list_registro <- list.files('data/', 'REGISTRO.txt$')
 
 bind_tables <- function (files_list) {
+  
   data <- data.frame(stringsAsFactors = F)
+  
   purrr::map_df(seq_along(files_list), function (i) {
+    
     file_path <- paste0('data/', files_list[i])
+    
     temp <- readr::read_delim(file      = file_path,
                               delim     = ';', 
                               locale = readr::locale(encoding = 'ISO-8859-1')
@@ -81,18 +112,24 @@ bind_tables <- function (files_list) {
       mutate(ANO = files_list[i] %>% 
                stringr::str_extract('^\\d{4}') %>% 
                as.numeric())
+    
     head(temp)
+    
     data <- bind_rows(data, temp)
+    
   })
+  
 }
 
 entrada <- bind_tables(files_list = files_list_entrada)
+
 readr::write_delim(x     = entrada, 
                    path  = 'binded_tables/entrada.txt', 
                    delim = ';', 
                    na    = '')
 
 registro <- bind_tables(files_list = files_list_registro)
+
 readr::write_delim(x     = registro, 
                    path  = 'binded_tables/registro.txt', 
                    delim = ';', 
